8f7718fea96adc47c267cfe3366e076478853359
cardissuer <cardissuer@gmail.com>
2025-01-10 12:26:33 -0600

    Test file updates 01-10-2025

--- src/Test/Testing.java
+++ src/Test/Testing.java
@@ -10,18 +10,27 @@ public class Testing {
 //    Output: {"cost", "DUE", "Stone", "cost1", "Stone1", "DUE1", "Stone2", "whole", "cost2"};
 
     public static void main(String[] args) {
-        String[] intArray = {"cost", "DUE", "Stone", "cost", "Stone", "DUE", "Stone", "whole", "cost"};
-        Arrays.sort(intArray);
-        int result[] = new int[intArray.length];
-        int count = 0;
-        for (int i = intArray.length; i > 0 ; i--) {
-            if (intArray[i] == intArray[i - 1]) {
-                ++count;
-                intArray[i + 1] = intArray[i - 1] + count;
-//            } else if (!(intArray[i] == intArray[i - 1])) {
-//                continue;
+//        String[] intArray = {"cost", "DUE", "Stone", "cost", "Stone", "DUE", "Stone", "whole", "cost"};
+//        Arrays.sort(intArray);
+//        int result[] = new int[intArray.length];
+//        int count = 0;
+//        for (int i = intArray.length; i > 0 ; i--) {
+//            if (intArray[i] == intArray[i - 1]) {
+//                ++count;
+//                intArray[i + 1] = intArray[i - 1] + count;
+////            } else if (!(intArray[i] == intArray[i - 1])) {
+////                continue;
+//            }
+//        }
+//another task
+        for (int i = 0; i < 3; i++) {
+            for (int j = 3; j >= 0; j--) {
+                if (i == j)
+                    continue;
+                System.out.println(i + " " + j);
             }
         }
 
     }
-}
+
+    }
