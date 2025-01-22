7087b4dfbf01de7ebfed9a72e31e1db5744bd005
cardissuer <cardissuer@gmail.com>
2025-01-22 09:41:37 -0600

    ReverseStringGurhan updates 01-22-2025

--- src/Strings/ReverseStringGurhan.java
+++ src/Strings/ReverseStringGurhan.java
@@ -12,5 +12,14 @@ public class ReverseStringGurhan {
             reversed = reversed + sentence.charAt(i);
         }
         System.out.println(reversed);
+        System.out.println(reverseMethod("jungle boogie"));
+    }
+
+    public static String reverseMethod (String word){
+        String reverseWord = "";
+        for (int i=word.length()-1;i>=0;i--){
+            reverseWord += word.charAt(i);
+        }
+        return reverseWord;
     }
 }
